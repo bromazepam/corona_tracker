17329ea4cf784d8ed1b16e0bf1d336f642dd14fb
bromazepam <graor95@gmail.com>
2021-02-09 23:50:45 +0100

    refactoring

--- src/main/java/com/corona/tracker/controller/MainController.java
+++ src/main/java/com/corona/tracker/controller/MainController.java
@@ -1,34 +1,27 @@
 package com.corona.tracker.controller;
 
 
+import com.corona.tracker.domain.Corona;
 import com.corona.tracker.service.CoronaServiceImpl;
-import com.fasterxml.jackson.core.JsonProcessingException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-import java.time.LocalDate;
+import java.util.List;
 
 @Slf4j
-@RestController
+@Controller
 @RequiredArgsConstructor
 public class MainController {
 
     private final CoronaServiceImpl coronaService;
 
     @GetMapping("/")
-    public String root(Model model) throws JsonProcessingException {
-        coronaService.populateDatabase();
-
-        return "mainTemplate";
-    }
-
-    @GetMapping("/2")
-    public String root2(Model model) throws JsonProcessingException {
-
-        model.addAttribute("corona", coronaService.findByLastUpdate(LocalDate.now().minusDays(1)));
+    public String root2(Model model) {
+        List<Corona> allStats = coronaService.getAllStats();
+        model.addAttribute("coronaData", allStats);
         return "mainTemplate";
     }
 }
